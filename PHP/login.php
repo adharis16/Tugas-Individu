341c753c4dbfa2890abdaa231c798228b15ca056
adharis16 <65618520+adharis16@users.noreply.github.com>
2020-05-30 00:31:13 +0700

    Add files via upload

---
+++ PHP/login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if(isset($_SESSION['email'])) {
+    echo '<script>window.location.replace("./index.php");</script>';
+} else {
+?>
+<center><h1>Form Login Tanpa Mysql</h1>
+<form action="./ceklogin.php" method="post">
+<input type="email" name="email" placeholder="Email" alt="email" required="required"/><br/>
+<input type="password" name="password" placeholder="Password" alt="password" required="required"/><br/><br/>
+<input type="submit" name="submit" value="Submit" alt="submit"/>
+</form><br/>
+</center>
+<?php } ?>
